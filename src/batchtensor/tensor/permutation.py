f04b6b0cba6bed58dd1cd0f53146bd3f3929e880
Thibaut Durand <durand.tibo@gmail.com>
2024-01-28 01:05:10 -0800

    Add `permute_along_batch/seq`

---
+++ src/batchtensor/tensor/permutation.py
@@ -0,0 +1,89 @@
+r"""Contain some functions to permute data in tensors."""
+
+from __future__ import annotations
+
+__all__ = ["permute_along_batch", "permute_along_seq"]
+
+from typing import TYPE_CHECKING
+
+from batchtensor.constants import BATCH_DIM, SEQ_DIM
+
+if TYPE_CHECKING:
+    import torch
+
+
+def permute_along_batch(tensor: torch.Tensor, permutation: torch.Tensor) -> torch.Tensor:
+    r"""Permute the tensor along the batch dimension.
+
+    Args:
+        tensor: The tensor to split.
+        permutation: The 1-D tensor containing the indices of the
+            permutation. The shape should match the batch dimension
+            of the tensor.
+
+    Returns:
+        The tensor with permuted data along the batch dimension.
+
+    Raises:
+        RuntimeError: if the shape of the permutation does not match
+            the batch dimension of the tensor.
+
+    Example usage:
+
+    ```pycon
+    >>> import torch
+    >>> from batchtensor.tensor import permute_along_batch
+    >>> tensor = torch.arange(10).view(5, 2)
+    >>> permute_along_batch(tensor, torch.tensor([2, 1, 3, 0, 4]))
+    tensor([[4, 5],
+            [2, 3],
+            [6, 7],
+            [0, 1],
+            [8, 9]])
+
+    ```
+    """
+    if permutation.shape[0] != tensor.shape[0]:
+        msg = (
+            f"permutation shape ({permutation.shape}) is not compatible with tensor shape "
+            f"({tensor.shape})"
+        )
+        raise RuntimeError(msg)
+    return tensor.index_select(dim=BATCH_DIM, index=permutation)
+
+
+def permute_along_seq(tensor: torch.Tensor, permutation: torch.Tensor) -> torch.Tensor:
+    r"""Permute the tensor along the sequence dimension.
+
+    Args:
+        tensor: The tensor to split.
+        permutation: The 1-D tensor containing the indices of the
+            permutation. The shape should match the sequence dimension
+            of the tensor.
+
+    Returns:
+        The tensor with permuted data along the sequence dimension.
+
+    Raises:
+        RuntimeError: if the shape of the permutation does not match
+            the sequence dimension of the tensor.
+
+    Example usage:
+
+    ```pycon
+    >>> import torch
+    >>> from batchtensor.tensor import permute_along_seq
+    >>> tensor = torch.arange(10).view(2, 5)
+    >>> permute_along_seq(tensor, torch.tensor([2, 1, 3, 0, 4]))
+    tensor([[4, 3, 2, 1, 0],
+            [9, 8, 7, 6, 5]])
+
+    ```
+    """
+    if permutation.shape[0] != tensor.shape[1]:
+        msg = (
+            f"permutation shape ({permutation.shape}) is not compatible with tensor shape "
+            f"({tensor.shape})"
+        )
+        raise RuntimeError(msg)
+    return tensor.index_select(dim=SEQ_DIM, index=permutation)
