556ba422ac5bb73bfb56f23faf130e258ab0110a
Thibaut Durand <durand.tibo@gmail.com>
2024-01-28 00:38:36 -0800

    Add `chunk_along_batch/seq` (#13)

--- src/batchtensor/tensor/__init__.py
+++ src/batchtensor/tensor/__init__.py
@@ -5,17 +5,21 @@ from __future__ import annotations
 __all__ = [
     "cat_along_batch",
     "cat_along_seq",
+    "chunk_along_batch",
+    "chunk_along_seq",
     "index_select_along_batch",
     "index_select_along_seq",
-    "slice_along_batch",
-    "slice_along_seq",
     "select_along_batch",
     "select_along_seq",
+    "slice_along_batch",
+    "slice_along_seq",
 ]
 
 from batchtensor.tensor.indexing import index_select_along_batch, index_select_along_seq
 from batchtensor.tensor.joining import cat_along_batch, cat_along_seq
 from batchtensor.tensor.slicing import (
+    chunk_along_batch,
+    chunk_along_seq,
     select_along_batch,
     select_along_seq,
     slice_along_batch,
