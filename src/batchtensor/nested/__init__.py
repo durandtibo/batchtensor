a617a67a3f579cf20a303dd95486be8dca1bab22
Thibaut Durand <durand.tibo@gmail.com>
2024-01-28 21:18:26 -0800

    Add `permute_along_batch/seq` for nested data

--- src/batchtensor/nested/__init__.py
+++ src/batchtensor/nested/__init__.py
@@ -7,7 +7,17 @@ __all__ = [
     "cat_along_seq",
     "index_select_along_batch",
     "index_select_along_seq",
+    "permute_along_batch",
+    "permute_along_seq",
+    "shuffle_along_batch",
+    "shuffle_along_seq",
 ]
 
 from batchtensor.nested.indexing import index_select_along_batch, index_select_along_seq
 from batchtensor.nested.joining import cat_along_batch, cat_along_seq
+from batchtensor.nested.permutation import (
+    permute_along_batch,
+    permute_along_seq,
+    shuffle_along_batch,
+    shuffle_along_seq,
+)
