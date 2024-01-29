0a2ab35babc42fdb3263a47ea519a442639baf40
Thibaut Durand <durand.tibo@gmail.com>
2024-01-28 19:01:09 -0800

    Add `index_select_along_batch/seq` for nested data

---
+++ src/batchtensor/nested/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain functions to manipulate nested data."""
+
+from __future__ import annotations
+
+__all__ = ["index_select_along_batch", "index_select_along_seq"]
+
+from batchtensor.nested.indexing import index_select_along_batch, index_select_along_seq
