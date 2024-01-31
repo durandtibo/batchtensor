0d3f3b7bbef45b1545a050fa54daa1c3754d8f2b
Thibaut Durand <durand.tibo@gmail.com>
2024-01-31 00:13:25 -0800

    Add `amax_along_batch/seq` for nested data

--- tests/unit/nested/test_reduction.py
+++ tests/unit/nested/test_reduction.py
@@ -54,6 +54,19 @@ def test_amax_along_batch_dict_keepdim_true(dtype: torch.dtype) -> None:
     )
 
 
+def test_amax_along_batch_nested() -> None:
+    assert objects_are_equal(
+        amax_along_batch(
+            {
+                "a": torch.arange(10).view(5, 2),
+                "b": torch.tensor([4, 3, 2, 1, 0]),
+                "c": [torch.tensor([5, 6, 7, 8, 9])],
+            }
+        ),
+        {"a": torch.tensor([8, 9]), "b": torch.tensor(4), "c": [torch.tensor(9)]},
+    )
+
+
 ####################################
 #     Tests for amax_along_seq     #
 ####################################
@@ -93,6 +106,19 @@ def test_amax_along_seq_dict_keepdim_true(dtype: torch.dtype) -> None:
             keepdim=True,
         ),
         {"a": torch.tensor([[4], [9]], dtype=dtype), "b": torch.tensor([[4]])},
+    )
+
+
+def test_amax_along_seq_nested() -> None:
+    assert objects_are_equal(
+        amax_along_seq(
+            {
+                "a": torch.arange(10).view(2, 5),
+                "b": torch.tensor([[4, 3, 2, 1, 0]]),
+                "c": [torch.tensor([[5, 6, 7, 8, 9]])],
+            }
+        ),
+        {"a": torch.tensor([4, 9]), "b": torch.tensor([4]), "c": [torch.tensor([9])]},
     )
 
 
@@ -138,6 +164,19 @@ def test_amin_along_batch_dict_keepdim_true(dtype: torch.dtype) -> None:
     )
 
 
+def test_amin_along_batch_nested() -> None:
+    assert objects_are_equal(
+        amin_along_batch(
+            {
+                "a": torch.arange(10).view(5, 2),
+                "b": torch.tensor([4, 3, 2, 1, 0]),
+                "c": [torch.tensor([5, 6, 7, 8, 9])],
+            }
+        ),
+        {"a": torch.tensor([0, 1]), "b": torch.tensor(0), "c": [torch.tensor(5)]},
+    )
+
+
 ####################################
 #     Tests for amin_along_seq     #
 ####################################
@@ -177,4 +216,17 @@ def test_amin_along_seq_dict_keepdim_true(dtype: torch.dtype) -> None:
             keepdim=True,
         ),
         {"a": torch.tensor([[0], [5]], dtype=dtype), "b": torch.tensor([[0]])},
+    )
+
+
+def test_amin_along_seq_nested() -> None:
+    assert objects_are_equal(
+        amin_along_seq(
+            {
+                "a": torch.arange(10).view(2, 5),
+                "b": torch.tensor([[4, 3, 2, 1, 0]]),
+                "c": [torch.tensor([[5, 6, 7, 8, 9]])],
+            }
+        ),
+        {"a": torch.tensor([0, 5]), "b": torch.tensor([0]), "c": [torch.tensor([5])]},
     )
