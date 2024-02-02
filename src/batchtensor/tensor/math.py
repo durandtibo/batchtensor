74b7cab070aee2f305d59077b0621e3646e57e07
Thibaut Durand <durand.tibo@gmail.com>
2024-02-01 21:23:43 -0800

    Add `cumprod_along_batch/seq` for tensor

--- src/batchtensor/tensor/math.py
+++ src/batchtensor/tensor/math.py
@@ -2,12 +2,80 @@ r"""Contain some mathematical functions for tensors."""
 
 from __future__ import annotations
 
-__all__ = ["cumsum_along_batch", "cumsum_along_seq"]
+__all__ = [
+    "cumprod_along_batch",
+    "cumprod_along_seq",
+    "cumsum_along_batch",
+    "cumsum_along_seq",
+]
 
-
-import torch
+from typing import TYPE_CHECKING
 
 from batchtensor.constants import BATCH_DIM, SEQ_DIM
+
+if TYPE_CHECKING:
+    import torch
+
+
+def cumprod_along_batch(tensor: torch.Tensor) -> torch.Tensor:
+    r"""Return the cumulative sum of elements of input in the batch
+    dimension.
+
+    Note:
+        This function assumes the batch dimension is the first
+            dimension.
+
+    Args:
+        tensor: The input tensor.
+
+    Returns:
+        The cumulative sum of elements of input in the batch
+            dimension.
+
+    Example usage:
+
+    ```pycon
+    >>> import torch
+    >>> from batchtensor.tensor import cumprod_along_batch
+    >>> tensor = torch.arange(10).view(5, 2)
+    >>> out = cumprod_along_batch(tensor)
+    >>> out
+    tensor([[   1,    2], [   3,    8], [  15,   48], [ 105,  384], [ 945, 3840]])
+
+    ```
+    """
+    return tensor.cumprod(dim=BATCH_DIM)
+
+
+def cumprod_along_seq(tensor: torch.Tensor) -> torch.Tensor:
+    r"""Return the cumulative sum of elements of input in the sequence
+    dimension.
+
+    Note:
+        This function assumes the sequence dimension is the second
+            dimension.
+
+    Args:
+        tensor: The input tensor.
+
+    Returns:
+        The cumulative sum of elements of input in the sequence
+            dimension.
+
+    Example usage:
+
+    ```pycon
+    >>> import torch
+    >>> from batchtensor.tensor import cumprod_along_seq
+    >>> tensor = torch.arange(10).view(2, 5)
+    >>> out = cumprod_along_seq(tensor)
+    >>> out
+    tensor([[    1,     2,     6,    24,   120],
+            [    6,    42,   336,  3024, 30240]])
+
+    ```
+    """
+    return tensor.cumprod(dim=SEQ_DIM)
 
 
 def cumsum_along_batch(tensor: torch.Tensor) -> torch.Tensor:
@@ -68,4 +136,4 @@ def cumsum_along_seq(tensor: torch.Tensor) -> torch.Tensor:
 
     ```
     """
-    return torch.cumsum(tensor, dim=SEQ_DIM)
+    return tensor.cumsum(dim=SEQ_DIM)
