e866bee3640a8eab3de14661648c7059784df372
Thibaut Durand <durand.tibo@gmail.com>
2025-12-27 15:53:25 -0800

    Add integration tests (#490)

---
+++ tests/integration/nested/test_conversion.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import torch
+from coola import objects_are_equal
+
+from batchtensor.nested import cumprod_along_batch
+
+#########################################
+#     Tests for cumprod_along_batch     #
+#########################################
+
+
+def test_cumprod_along_batch_dict() -> None:
+    assert objects_are_equal(
+        cumprod_along_batch(
+            {
+                "a": torch.tensor(
+                    [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]],
+                ),
+                "b": torch.tensor([4, 3, 2, 1, 0]),
+            }
+        ),
+        {
+            "a": torch.tensor([[1, 2], [3, 8], [15, 48], [105, 384], [945, 3840]]),
+            "b": torch.tensor([4, 12, 24, 24, 0]),
+        },
+    )
